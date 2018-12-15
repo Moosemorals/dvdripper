ad0b698b12a7795c2ee4d12a893a8151f670bf9a
Osric Wilkinson <osric@fluffypeople.com>
2018-12-15 08:40:53 +0000

    Started websockets, husband needs his machine

--- httpd.go
+++ httpd.go
@@ -3,8 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  8096,
+	WriteBufferSize: 8096,
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Websocket: Upgrade failed: %v", err)
+		return
+	}
+}
+
 func startServer() {
-	log.Print(http.ListenAndServe(":8080", http.FileServer(http.Dir("wwwroot"))))
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/ws", wsHandler)
+	mux.Handle("/", http.FileServer(http.Dir("wwwroot")))
+
+	log.Print(http.ListenAndServe(":8080", mux))
 }
