fc8826287d74a632284eed0e20cfdeca76d7a7d9
Osric Wilkinson <osric@fluffypeople.com>
2018-12-15 02:08:03 +0000

    Well, that's mplayer. Time for web, I guess

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -35,10 +37,20 @@ type DVD struct {
 	LongestTrack int
 	// Titles is a slice of all the titles on the disk
 	Titles []DVDTrack
+	// ParseOK is a bool that's true if there were no parsing errors
+	ParseOK bool
 }
 
-func startCmd(path string, args ...string) (cmd *exec.Cmd, stdout io.ReadCloser, err error) {
+// DVDProgress reports the progress of the rip
+type DVDProgress struct {
+	// Bytes is the number of bytes written to disk
+	Bytes int
+	// Percent is the (rough) percent of the track ripped
+	Percent float64
+}
 
+// A thin wrapper around os.exec
+func startCmd(path string, args ...string) (cmd *exec.Cmd, stdout io.ReadCloser, err error) {
 	cmd = exec.Command(path, args...)
 
 	stdout, err = cmd.StdoutPipe()
@@ -62,7 +74,9 @@ func lsdvd() DVD {
 	reTitle := regexp.MustCompile(`^Title: (\d\d), Length: (\d\d:\d\d:\d\d.\d\d\d) Chapters: (\d\d), Cells: (\d\d), Audio streams: (\d\d), Subpictures: (\d\d)$`)
 	reLongestTrack := regexp.MustCompile(`^Longest track: (\d\d)$`)
 
-	disk := DVD{}
+	disk := DVD{
+		ParseOK: true,
+	}
 
 	scanner := bufio.NewScanner(stdout)
 scan:
@@ -72,17 +86,13 @@ scan:
 		switch {
 		case reTitle.MatchString(line):
 			match := reTitle.FindStringSubmatch(line)
-
 			trackLength := match[2]
-
 			numberFields := append(match[1:2], match[3:]...)
-
-			log.Printf("Number fileds %+v", numberFields)
-
 			numbers := []int{}
 			for _, s := range numberFields {
 				n, err := strconv.Atoi(s)
 				if err != nil {
+					disk.ParseOK = false
 					log.Printf("WARN: Can't parse line %s: %v", line, err)
 					continue scan
 				}
@@ -105,24 +115,123 @@ scan:
 			match := reLongestTrack.FindStringSubmatch(line)
 			disk.LongestTrack, err = strconv.Atoi(match[1])
 			if err != nil {
+				disk.ParseOK = false
 				log.Printf("WARN: Can't convert %s to int: %v", match[1], err)
+				continue scan
 			}
 		default:
 			log.Printf("Unknown line %s", line)
+			disk.ParseOK = false
+			continue scan
 		}
-
 	}
 
+	// Wait for the command to finish.
 	cmd.Wait()
 
 	return disk
+}
 
+func startMplayer(path string, args ...string) (cmd *exec.Cmd, stderr io.ReadCloser, err error) {
+	cmd = exec.Command(path, args...)
+
+	stderr, err = cmd.StderrPipe()
+	if err != nil {
+		return
+	}
+	if err = cmd.Start(); err != nil {
+		return
+	}
+	return
+}
+
+// These two functions are adappted from the go library scanlines
+// under a modified BSD licence
+// See https://golang.org/src/bufio/scan.go?s=11799:11877#L335
+
+// dropCR drops a terminal \r fron the data
+func dropCR(data []byte) []byte {
+	if len(data) > 0 && data[len(data)-1] == '\r' {
+		return data[0 : len(data)-1]
+	}
+	return data
+}
+
+func scanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// Found a newline
+		return i + 1, dropCR(data), nil
+	}
+	if i := bytes.IndexByte(data, '\r'); i >= 0 {
+		// Found a newline
+		return i + 1, dropCR(data), nil
+	}
+
+	// Request more data
+	return 0, nil, nil
+}
+
+func mplayer(track int, dest string) error {
+
+	src := fmt.Sprintf("dvd://%d", track)
+
+	cmd, stdout, err := startCmd("/usr/bin/mplayer", "-quiet", "-nocache", "-dumpstream", src, "-dumpfile", dest)
+
+	if err != nil {
+		return err
+	}
+
+	out := json.NewEncoder(os.Stdout)
+
+	dumpRE := regexp.MustCompile(`^dump: (\d+) bytes written \(~(\d+\.\d+)%\)$`)
+
+	scanner := bufio.NewScanner(stdout)
+	scanner.Split(scanLines)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if dumpRE.MatchString(line) {
+			match := dumpRE.FindStringSubmatch(line)
+
+			bytes, err := strconv.Atoi(match[1])
+			if err != nil {
+				log.Printf("WARN: %+v", err)
+				continue
+			}
+
+			percent, err := strconv.ParseFloat(match[2], 64)
+			if err != nil {
+				log.Printf("WARN: %+v", err)
+				continue
+			}
+
+			progress := DVDProgress{
+				Bytes:   bytes,
+				Percent: percent,
+			}
+
+			out.Encode(progress)
+			fmt.Println()
+		}
+	}
+
+	cmd.Wait()
+	return nil
+}
+
+func diskToJSON() {
+	disk := lsdvd()
+	json.NewEncoder(os.Stdout).Encode(disk)
 }
 
 func main() {
 	log.Print("Starting")
-	disk := lsdvd()
 
-	json.NewEncoder(os.Stdout).Encode(disk)
-
+	err := mplayer(3, "/tmp/junk.mpg")
+	if err != nil {
+		log.Fatalf("Can't run mplayer: %v", err)
+	}
 }
